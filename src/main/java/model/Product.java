dc1a15616aa10d455250cb67481dab8fa3171f13
Darkinator <razonado.john1@gmail.com>
2024-04-26 21:08:56 +0800

    Added: Images of initial products
    These are all the based images of initial products of the system. This is composed of their original .webp files and converted .png file.

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,57 @@
+package  model;
+class Product{
+    private String name;
+    private String description;
+    private String category;
+    private  Double price;
+    private String image_path;
+    private  int stock;
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImage_path() {
+        return image_path;
+    }
+
+    public void setImage_path(String image_path) {
+        this.image_path = image_path;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
