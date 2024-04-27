8f59c59123c67c2c15e5435b175a00ae6e0d0a50
Darkinator <razonado.john1@gmail.com>
2024-04-27 21:49:37 +0800

    Added: Temporary cart system
    This part is still questionable if this needed to be implemented at all.

---
+++ src/main/java/model/Cart.java
@@ -0,0 +1,67 @@
+package model;
+
+public class Cart {
+    private int id;
+    private int userId;
+    private int productId;
+    private String productName;
+    private double cost;
+    private int quantity;
+    private boolean isBought;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isBought() {
+        return isBought;
+    }
+
+    public void setBought(boolean bought) {
+        isBought = bought;
+    }
+}
