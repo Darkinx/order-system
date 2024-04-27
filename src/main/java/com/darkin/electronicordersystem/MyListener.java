b8c46b6a9ee498055a99e173c5cba0e453568cf2
Darkinator <razonado.john1@gmail.com>
2024-04-27 21:52:30 +0800

    Added: Listener interface
    Just to pass the product information to fetch by the product view.

---
+++ src/main/java/com/darkin/electronicordersystem/MyListener.java
@@ -0,0 +1,8 @@
+package com.darkin.electronicordersystem;
+
+import model.Product;
+
+//Basic interface to carry the product item
+public interface MyListener  {
+    public  void onClickListener(Product product);
+}
